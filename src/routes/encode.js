b80074394ab99f9e57ab3ac3dc3fd1755bb350bc
maximilianheidenreich <github@maximilian-heidenreich.de>
2021-04-28 10:48:22 +0200

    💡 Adds info comment

--- src/routes/encode.js
+++ src/routes/encode.js
@@ -3,10 +3,10 @@ import { encodeUnicode } from "../../lib/unicode";
 export async function post({ body }) {
     const { message } = body;
 
+    // Return err or encoded data
     if (message === undefined) return { status: 400 }
-
     const encoded = encodeUnicode(message);
-
+    
     return {
         body: {
             data: encoded
